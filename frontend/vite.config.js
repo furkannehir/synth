fa62fa14f113fdf1c808bb30245c0d7a6460e0cb
Furkan Nehir <furkannehir1992@gmail.com>
2026-04-02 20:41:02 +0300

    desktop application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  clearScreen: false,
   server: {
+    port: 5173,
+    strictPort: true,
+    host: !!process.env.TAURI_ENV_DEBUG ? '0.0.0.0' : false,
     proxy: {
       '/api': 'http://localhost:5000',
       '/health': 'http://localhost:5000',
     },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_'],
+  build: {
+    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
 })
